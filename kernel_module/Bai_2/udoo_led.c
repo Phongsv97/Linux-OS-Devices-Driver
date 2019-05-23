a5394d161d5b369b756e5de2e4dc9a0f3e4dac16
Phongsv97 <phongsv1731997@gmail.com>
2019-05-23 09:56:12 +0700

    push udoo_led.c

--- kernel_module/Bai_2/udoo_led.c
+++ kernel_module/Bai_2/udoo_led.c
@@ -6,6 +6,11 @@
 #define DRIVER_DESC     "UDOO GPIO BLINK LED PIN25"
 #define DRIVER_AUTHOR   "Phong LT9 <phongxxx@gmail.com>"
 
+/*define MUX*/
+#define MUX         0x020E0078h
+#define GPIO1_IO25  0x05h
+
+/*define GPIO reg*/
 #define GPIO_BASE 0x0209C000h
 
 #define OFFSET_GPIO1_DR     0x00h
@@ -17,6 +22,7 @@
 #define LED_PIN 25
 
 static void __iomem *io;
+static void __iomem *gpio;
 static unsigned long temp;
 
 
@@ -24,30 +30,36 @@ static int __init init_led(void)
 {
     pr_info("hello kernel \n");
 
-    io = ioremap(GPIO_BASE, 0x100);
+    /*set up mux -- enable GPIO1*/
+    gpio = ioremap(MUX, 0x100);
+    if(gpio == NULL){
+        pr_alert("fail map base address\n");
+        return -1;
+    }  
+    temp = ioread32(gpio);
+    pr_info("before config reg : %ld", temp);
+    temp = temp & (~GPIO1_IO25);
+    temp = temp | ( GPIO1_IO25);
+    iowrite32(temp, gpio);
 
+    /*set up gpio INPUT - OUTPUT*/
+    io = ioremap(GPIO_BASE, 0x100);
     if(io == NULL){
         pr_alert("fail map base address\n");
         return -1;
     }
-
-    /*write config mod for pin 25*/
     temp = ioread32(io + GPIO1_GDIR_OFFSET);
     pr_info("before config reg : %ld", temp);
-
-    /*set config mode output for pa17*/	
-	temp = temp & (~(1 << LED_PIN));
-	
-    /**/
-	temp = iowrite32(temp, (io + GPIO1_GDIR_OFFSET)); 
-	pr_info("after config reg %ld\n", temp);
+    temp = temp | (GPIO_OUTPUT << LED_PIN);
+    iowrite32(temp, io + OFFSET_GPIO1_GDIR);
+    pr_info("after data reg : %ld\n", temp);
 
 	/*turn on led*/
 	temp = ioread32(io + OFFSET_GPIO1_DR);
-	pr_info("before data reg %ld\n", temp);
+	pr_info("before data reg : %ld\n", temp);
 	temp |= ( 1 << LED_PIN);
 	iowrite32(temp , (io +OFFSET_GPIO1_DR));
-	pr_info("after data reg %ld\n", temp);
+	pr_info("after data reg : %ld\n", temp);
 
     return 0;
 }
@@ -56,8 +68,10 @@ static void __exit exit_led(void)
 {
     /*turn off led*/
 	temp = ioread32(io + OFFSET_GPIO1_DR);
+    pr_info("before data reg : %ld\n", temp);
 	temp &= ~( 1 << LED_PIN);
 	iowrite32(temp , (io +OFFSET_GPIO1_DR));
+    pr_info("after data reg : %ld\n", temp);
 	pr_info("exit module");	
 }
 
